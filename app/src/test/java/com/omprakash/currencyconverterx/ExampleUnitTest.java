46b988a9b5176aabc320301269ea14f3ad02967c
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-07-10 13:07:42 +0530

    i implemented test case functionality for the currency Api

--- app/src/test/java/com/omprakash/currencyconverterx/ExampleUnitTest.java
+++ app/src/test/java/com/omprakash/currencyconverterx/ExampleUnitTest.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +19,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+
+    public void fetchCurrencies () throws IOException {
+        CurrencyApi currencyApi = new CurrencyApi();
+        CurrencyService currencyService = currencyApi.createCurrencyService();
+        Call<Currency> call = currencyService.fetchCurrencies();
+        Currency currency = call.execute().body();
+        assertNotNull(currency);
+        System.out.println(new Gson().toJson(currency));
     }
 }
