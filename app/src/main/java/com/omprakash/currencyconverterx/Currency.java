d76313a1190e1bb4a465809575655320a910f7c4
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-07-10 12:34:57 +0530

    created a model class

---
+++ app/src/main/java/com/omprakash/currencyconverterx/Currency.java
@@ -0,0 +1,74 @@
+package com.omprakash.currencyconverterx;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Currency {
+
+    @SerializedName("AUD")
+    private Double aud;
+    @SerializedName("BGN")
+    private Double bgn;
+    @SerializedName("BRL")
+    private Double brl;
+    @SerializedName("CAD")
+    private Double cad;
+    @SerializedName("CHF")
+    private Double chf;
+    @SerializedName("CNY")
+    private Double cny;
+    @SerializedName("CZK")
+    private Double czk;
+    @SerializedName("DKK")
+    private Double dkk;
+    @SerializedName("EUR")
+    private Double eur;
+    @SerializedName("GBP")
+    private Double gbp;
+    @SerializedName("HKD")
+    private Double hkd;
+    @SerializedName("HRK")
+    private Double hrk;
+    @SerializedName("HUF")
+    private Double huf;
+    @SerializedName("IDR")
+    private Double idr;
+    @SerializedName("ILS")
+    private Double ils;
+    @SerializedName("INR")
+    private Double inr;
+    @SerializedName("ISK")
+    private Double isk;
+    @SerializedName("JPY")
+    private Double jpy;
+    @SerializedName("KRW")
+    private Double krw;
+    @SerializedName("MXN")
+    private Double mxn;
+    @SerializedName("MYR")
+    private Double myr;
+    @SerializedName("NOK")
+    private Double nok;
+    @SerializedName("NZD")
+    private Double nzd;
+    @SerializedName("PHP")
+    private Double php;
+    @SerializedName("PLN")
+    private Double pln;
+    @SerializedName("RON")
+    private Double ron;
+    @SerializedName("RUB")
+    private Double rub;
+    @SerializedName("SEK")
+    private Double sek;
+    @SerializedName("SGD")
+    private Double sgd;
+    @SerializedName("THB")
+    private Double thb;
+    @SerializedName("TRY")
+    private Double trye;
+    @SerializedName("USD")
+    private Double usd;
+    @SerializedName("ZAR")
+    private Double zar;
+}
+
